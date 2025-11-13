d2efd5921b1eb874b174d57d7dc1690230f051d3
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2025-11-13 15:46:08 -0300

    Add overflow-x-hidden class to body for improved layout handling

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,10 +22,10 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased bg-gray-100 dark:bg-gray-900">
+    <body class="font-sans text-gray-900 antialiased bg-gray-100 dark:bg-gray-900 overflow-x-hidden">
         <x-toast-container />
 
-        <div class="w-screen min-h-screen flex flex-col gap-6 sm:justify-center items-center pt-6 sm:pt-0">
+        <div class="w-full min-h-[100dvh] flex flex-col gap-6 sm:justify-center items-center pt-6 sm:pt-0">
             <div class="pt-4">
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
